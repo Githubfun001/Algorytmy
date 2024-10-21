89c59cab8ff8719e3b04c22bede3995b099fe4b8
Filip Nowak <147511167+Githubfun001@users.noreply.github.com>
2024-10-21 12:54:10 +0200

    Add files via upload

---
+++ WinFormsApp2/List.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    internal class List
+    {
+        public Node head;
+        public Node tail;
+        public int count=0;
+
+        public void AddFirst(int liczba)
+        {
+
+        }
+        
+        public void AddLast(int liczba)
+        {
+            Node add = new Node(liczba);
+            this.tail.next = add;
+            add.prev = this.tail;
+            this.tail = add;
+            this.count++;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
