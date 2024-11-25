cc34be8ae25e613c86ae027c21da84b675482adf
Filip Nowak <147511167+Githubfun001@users.noreply.github.com>
2024-11-25 12:54:33 +0100

    Add files via upload

---
+++ WinFormsApp5/Graf1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp5
+{
+    internal class Graf1
+    {
+        List<NodeG1> nodes = new();
+        List<Edge> edges = new();
+
+        Graf1(Edge k)
+        {
+            Add(k);
+            this.edges.Add(k);
+        }
+
+        public void Add(Edge k)
+        {
+            if (this.nodes.Contains(k.start))
+                this.nodes.Add(k.start);
+
+            if (this.nodes.Contains(k.end))
+                this.nodes.Add(k.end);
+        }
+
+        public int IleNowychWezlow(Edge k)
+        {
+            int wynik = 0;
+            if (!nodes.Contains(k.start))
+                wynik++;
+
+            if (!nodes.Contains(k.end))
+                wynik++;
+
+            return wynik;
+        }
+
+        public void Join(Graf1 g1)
+        {
+            foreach(Edge edge in g1.edges)
+            {
+                Add(edge);
+            }
+        }
+    }
+}
---
+++ WinFormsApp5/NodeG1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp5
+{
+    internal class NodeG1
+    {
+        int data;
+    }
+}
