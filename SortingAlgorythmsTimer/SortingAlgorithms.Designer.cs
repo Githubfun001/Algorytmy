3dcfec9e56677a26bba8a548607749b7de04fa0a
Githubfun001 <filip.nowak3@wp.pl>
2025-09-08 16:41:16 +0200

    App1 finished

---
+++ SortingAlgorythmsTimer/SortingAlgorithms.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WinFormsApp1
+{
+    partial class SortingAlgorithms
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numberDisplay = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            randomNumberCount = new NumericUpDown();
+            userInput = new TextBox();
+            timeDisplay = new Label();
+            ((System.ComponentModel.ISupportInitialize)randomNumberCount).BeginInit();
+            SuspendLayout();
+            // 
+            // numberDisplay
+            // 
+            numberDisplay.AutoSize = true;
+            numberDisplay.Location = new Point(136, 259);
+            numberDisplay.Name = "numberDisplay";
+            numberDisplay.Size = new Size(253, 20);
+            numberDisplay.TabIndex = 1;
+            numberDisplay.Text = "Inputted numbers will be shown here";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(168, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Accept";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += UserInputButton;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(626, 126);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Randomize";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += RandomInputButton;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(63, 364);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "BubbleSort";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += BubbleSortButton;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(236, 364);
+            button4.Name = "button4";
+            button4.Size = new Size(111, 29);
+            button4.TabIndex = 5;
+            button4.Text = "InsertionSort";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += InsertionSortButton;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(419, 364);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 6;
+            button5.Text = "MergeSort";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += MergeSortButton;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(581, 364);
+            button6.Name = "button6";
+            button6.Size = new Size(107, 29);
+            button6.TabIndex = 7;
+            button6.Text = "CountingSort";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += CountingSortButton;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(752, 364);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 8;
+            button7.Text = "QuickSort";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += QuickSortButton;
+            // 
+            // randomNumberCount
+            // 
+            randomNumberCount.Location = new Point(594, 63);
+            randomNumberCount.Name = "randomNumberCount";
+            randomNumberCount.Size = new Size(150, 27);
+            randomNumberCount.TabIndex = 10;
+            // 
+            // userInput
+            // 
+            userInput.Location = new Point(89, 62);
+            userInput.Name = "userInput";
+            userInput.Size = new Size(252, 27);
+            userInput.TabIndex = 11;
+            // 
+            // timeDisplay
+            // 
+            timeDisplay.AutoSize = true;
+            timeDisplay.Location = new Point(136, 475);
+            timeDisplay.Name = "timeDisplay";
+            timeDisplay.Size = new Size(247, 20);
+            timeDisplay.TabIndex = 12;
+            timeDisplay.Text = "Time elapsed will be displayed here";
+            // 
+            // SortingAlgorithms
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(919, 556);
+            Controls.Add(timeDisplay);
+            Controls.Add(userInput);
+            Controls.Add(randomNumberCount);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(numberDisplay);
+            Name = "SortingAlgorithms";
+            Text = "Sorting Algorithms";
+            ((System.ComponentModel.ISupportInitialize)randomNumberCount).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label numberDisplay;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private NumericUpDown randomNumberCount;
+        private TextBox userInput;
+        private Label timeDisplay;
+    }
+}
---
+++ SortingAlgorythmsTimer/SortingAlgorithms.cs
@@ -0,0 +1,249 @@
+﻿using System.Diagnostics;
+
+namespace WinFormsApp1
+{
+    public partial class SortingAlgorithms : Form
+    {
+        Random random = new Random();
+        private int[] currentNumbers;
+
+        public SortingAlgorithms()
+        {
+            InitializeComponent();
+
+            randomNumberCount.Minimum = 1;
+            randomNumberCount.Maximum = 1000000;
+            randomNumberCount.Value = 100;
+            userInput.PlaceholderText = "eg. 10,5,20,1,19";
+        }
+        private void UserInputButton(object sender, EventArgs e)
+        {
+            string input = userInput.Text;
+
+            string[] inputArray = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            if (inputArray.All(s => int.TryParse(s.Trim(), out _)))
+            {
+                currentNumbers = inputArray
+                    .Select(s => int.Parse(s.Trim()))
+                    .ToArray();
+
+                UpdateLabelOutput();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Input only numbers separated by comma",
+                    "Wrong Format",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+            }
+        }
+
+        private void RandomInputButton(object sender, EventArgs e)
+        {
+            int amount = (int)randomNumberCount.Value;
+
+            int minRand = 0;
+            int maxRand = 10000;
+
+            currentNumbers = new int[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                currentNumbers[i] = random.Next(minRand, maxRand);
+            }
+
+            UpdateLabelOutput();
+        }
+
+        private void UpdateLabelOutput()
+        {
+            int maxDisplay = 15;
+            if (currentNumbers.Length <= maxDisplay)
+            {
+                numberDisplay.Text = "Numbers: " + string.Join(", ", currentNumbers);
+            }
+            else
+            {
+                numberDisplay.Text = "Numbers: " + string.Join(", ", currentNumbers.Take(maxDisplay)) + " ...";
+            }
+        }
+
+        private void BubbleSortButton(object sender, EventArgs e)
+        {
+            SortAndMeasureTime(BubbleSort, "Bubble Sort");
+        }
+        private void InsertionSortButton(object sender, EventArgs e)
+        {
+            SortAndMeasureTime(InsertionSort, "Insertion Sort");
+        }
+        private void MergeSortButton(object sender, EventArgs e)
+        {
+            SortAndMeasureTime(MergeSort, "Merge Sort");
+        }
+        private void CountingSortButton(object sender, EventArgs e)
+        {
+            SortAndMeasureTime(CountingSort, "Counting Sort");
+        }
+        private void QuickSortButton(object sender, EventArgs e)
+        {
+            SortAndMeasureTime(QuickSort, "Quick Sort");
+        }
+        private void SortAndMeasureTime(Func<int[], int[]> sortingAlgorithm, string algorithmName)
+        {
+            if (currentNumbers == null || currentNumbers.Length == 0) return;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            currentNumbers = sortingAlgorithm(currentNumbers);
+
+            stopwatch.Stop();
+
+            UpdateLabelOutput();
+
+            long elapsedTicks = stopwatch.ElapsedTicks;
+            double elapsedMilliseconds = (double)elapsedTicks / Stopwatch.Frequency * 1000; // Convert to ms
+
+            timeDisplay.Text = $"{algorithmName} - Time elapsed: {elapsedMilliseconds:F3} ms";
+        }
+
+        private int[] MergeSort(int[] array)
+        {
+            if (array.Length <= 1)
+                return array;
+
+            int mid = array.Length / 2;
+            int[] left = array.Take(mid).ToArray();
+            int[] right = array.Skip(mid).ToArray();
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        private int[] Merge(int[] left, int[] right)
+        {
+            int[] result = new int[left.Length + right.Length];
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                    result[k++] = left[i++];
+                else
+                    result[k++] = right[j++];
+            }
+
+            while (i < left.Length)
+                result[k++] = left[i++];
+            while (j < right.Length)
+                result[k++] = right[j++];
+
+            return result;
+        }
+        private int[] BubbleSort(int[] array)
+        {
+            int n = array.Length;
+            bool flag;
+            for (int i = 0; i < n - 1; i++)
+            {
+                flag = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int tempVar = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tempVar;
+                        flag = true;
+                    }
+                }
+                if (!flag) break;
+            }
+            return array;
+        }
+        private int[] InsertionSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+            return array;
+        }
+        private int[] CountingSort(int[] array)
+        {
+            int max = array.Max();
+            int min = array.Min();
+            int n = max - min + 1;
+
+            int[] countArray = new int[n];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                countArray[array[i] - min]++;
+            }
+            for (int i = 1; i < n; i++)
+            {
+                countArray[i] += countArray[i - 1];
+            }
+
+            int[] outputArray = new int[array.Length];
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                outputArray[countArray[array[i] - min] - 1] = array[i];
+                countArray[array[i] - min]--;
+            }
+            return outputArray;
+        }
+        private int[] QuickSort(int[] array)
+        {
+            QuickSortStart(array, 0, array.Length - 1);
+            return array;
+        }
+
+        private void QuickSortStart(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                QuickSortStart(array, low, pivotIndex - 1);
+                QuickSortStart(array, pivotIndex + 1, high);
+            }
+        }
+
+        private int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    Swap(array, i, j);
+                    i++;
+                }
+            }
+            Swap(array, i, high);
+            return i;
+        }
+        private void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
